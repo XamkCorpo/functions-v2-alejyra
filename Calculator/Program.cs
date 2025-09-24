5149144b6b4f3e203b92fd9e5805748b3e46d486
aleksi <aleksi.jyrakoski@gmail.com>
2025-09-24 14:12:54 +0300

    Laskutoimituksen valinta

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ChooseOperation();
         }
+
+        static int ChooseOperation()
+        {
+            Console.WriteLine("Choose an operation");
+            Console.WriteLine("1: addition");
+            Console.WriteLine("2: substraction");
+            Console.WriteLine("3: multiplication");
+            Console.WriteLine("4: division");
+
+            while (true)
+            {
+                string? input = Console.ReadLine();
+                if (int.TryParse(input, out int Operation) && Operation > 0 && Operation <= 4)
+                    return Operation;
+                else
+                    Console.WriteLine("Choose a number between 1 - 4");
+            }
+
+
+        }
+
     }
+
+
 }
